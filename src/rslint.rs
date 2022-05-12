f707e1a8cbbfb3dbf25b0aed3a0580f9b23baf9b
Arpad Borsos <swatinem@swatinem.de>
2022-05-12 10:02:34 +0200

    fix and simplify sourcecontext, split into files

---
+++ src/rslint.rs
@@ -0,0 +1,90 @@
+use std::ops::Range;
+
+use rslint_parser::{ast, AstNode, SyntaxKind, SyntaxNode, SyntaxNodeExt, TextRange};
+
+fn convert_text_range(range: TextRange) -> Range<u32> {
+    range.start().into()..range.end().into()
+}
+
+pub fn parse_with_rslint(src: &str) -> Vec<(Range<u32>, Option<String>)> {
+    let parse =
+        //rslint_parser::parse_with_syntax(src, 0, rslint_parser::FileKind::TypeScript.into());
+        rslint_parser::parse_text(src, 0);
+
+    let syntax = parse.syntax();
+    //dbg!(&syntax);
+
+    let mut ranges = vec![];
+
+    for node in syntax.descendants() {
+        if let Some(fn_decl) = node.try_to::<ast::FnDecl>() {
+            let name = fn_decl
+                .name()
+                .map(|n| n.text())
+                .or_else(|| find_fn_name_from_ctx(&node));
+
+            ranges.push((convert_text_range(node.text_range()), name));
+        } else if let Some(fn_expr) = node.try_to::<ast::FnExpr>() {
+            let name = fn_expr
+                .name()
+                .map(|n| n.text())
+                .or_else(|| find_fn_name_from_ctx(&node));
+
+            ranges.push((convert_text_range(node.text_range()), name));
+        } else if node.is::<ast::ArrowExpr>() {
+            let name = find_fn_name_from_ctx(&node);
+
+            ranges.push((convert_text_range(node.text_range()), name));
+        }
+
+        // TODO: method, constructor
+
+        /*match node.kind() {
+            SyntaxKind::METHOD => todo!(),
+            SyntaxKind::CONSTRUCTOR => todo!(),
+            _ => todo!(),
+        }*/
+    }
+
+    ranges
+}
+
+fn find_fn_name_from_ctx(node: &SyntaxNode) -> Option<String> {
+    // the node itself is the first "ancestor"
+    for parent in node.ancestors().skip(1) {
+        // break on syntax that itself starts a scope
+        match parent.kind() {
+            SyntaxKind::FN_DECL
+            | SyntaxKind::FN_EXPR
+            | SyntaxKind::ARROW_EXPR
+            | SyntaxKind::METHOD
+            | SyntaxKind::CONSTRUCTOR => return None,
+            _ => {}
+        }
+        if let Some(assign_expr) = parent.try_to::<ast::AssignExpr>() {
+            if let Some(ast::PatternOrExpr::Expr(expr)) = assign_expr.lhs() {
+                return Some(text_of_node(expr.syntax()));
+            }
+        }
+        if let Some(decl) = parent.try_to::<ast::Declarator>() {
+            if let Some(ast::Pattern::SinglePattern(sp)) = decl.pattern() {
+                return sp.name().map(|n| n.text());
+            }
+        }
+    }
+    None
+}
+
+fn text_of_node(node: &SyntaxNode) -> String {
+    use std::fmt::Write;
+
+    let mut s = String::new();
+    for node in node
+        .descendants_with_tokens()
+        .filter_map(|x| x.into_token().filter(|token| !token.kind().is_trivia()))
+    {
+        let _ = write!(&mut s, "{}", node.text());
+    }
+
+    s
+}
---
+++ src/source.rs
@@ -0,0 +1,144 @@
+/// A line/column source position.
+#[derive(Copy, Clone, Debug, PartialEq, PartialOrd)]
+pub struct SourcePosition {
+    /// Line in the source file, 0-based.
+    line: u32,
+    /// Column in the source file, 0-based.
+    ///
+    /// The column is given in UTF-16 code points.
+    column: u32,
+}
+
+impl SourcePosition {
+    /// Create a new SourcePosition with the given line/column.
+    pub fn new(line: u32, column: u32) -> Self {
+        Self { line, column }
+    }
+}
+
+/// A Source Context allowing fast access to lines and line/column <-> byte offset remapping.
+pub struct SourceContext<T> {
+    src: T,
+    line_offsets: Vec<u32>,
+}
+
+/// An Error that can happen when building a [`SourceContext`].
+#[derive(Debug)]
+pub struct SourceContextError(());
+
+impl<T: AsRef<str>> SourceContext<T> {
+    /// Construct a new Source Context from the given `src` buffer.
+    pub fn new(src: T) -> Result<Self, SourceContextError> {
+        let buf = src.as_ref();
+        let buf_ptr = buf.as_ptr();
+        let mut line_offsets: Vec<u32> = buf
+            .lines()
+            .map(|line| unsafe { line.as_ptr().offset_from(buf_ptr) as usize }.try_into())
+            .collect::<Result<Vec<_>, _>>()
+            .map_err(|_| SourceContextError(()))?;
+        line_offsets.push(buf.len().try_into().map_err(|_| SourceContextError(()))?);
+        Ok(Self { src, line_offsets })
+    }
+
+    /// Get the `nth` line of the source, 0-based.
+    pub fn get_line(&self, nth: u32) -> Option<&str> {
+        let from = self.line_offsets.get(nth as usize).copied()? as usize;
+        let to = self.line_offsets.get(nth as usize + 1).copied()? as usize;
+        self.src.as_ref().get(from..to)
+    }
+
+    /// Converts a byte offset into the source to the corresponding line/column.
+    ///
+    /// The column is given in UTF-16 code points.
+    pub fn offset_to_position(&self, offset: u32) -> Option<SourcePosition> {
+        let line_no = match self.line_offsets.binary_search(&offset) {
+            Ok(line) => line,
+            Err(0) => 0, // this is pretty much unreachable since the first offset is 0
+            Err(line) => line - 1,
+        };
+
+        let mut byte_offset = self.line_offsets.get(line_no).copied()? as usize;
+        let to = self.line_offsets.get(line_no + 1).copied()? as usize;
+
+        let line = self.src.as_ref().get(byte_offset..to)?;
+
+        let line_no = line_no.try_into().ok()?;
+        let mut utf16_offset = 0;
+        for c in line.chars() {
+            if byte_offset >= offset as usize {
+                return Some(SourcePosition::new(line_no, utf16_offset.try_into().ok()?));
+            }
+
+            utf16_offset += c.len_utf16();
+            byte_offset += c.len_utf8();
+        }
+
+        None
+    }
+
+    /// Converts the given line/column to the corresponding byte offset inside the source.
+    pub fn position_to_offset(&self, position: SourcePosition) -> Option<u32> {
+        let SourcePosition { line, column } = position;
+
+        let from = self.line_offsets.get(line as usize).copied()? as usize;
+        let to = self.line_offsets.get(line as usize + 1).copied()? as usize;
+
+        let line = self.src.as_ref().get(from..to)?;
+
+        let mut byte_offset = from;
+        let mut utf16_offset = 0;
+        let column = column as usize;
+        for c in line.chars() {
+            if utf16_offset >= column {
+                return byte_offset.try_into().ok();
+            }
+            utf16_offset += c.len_utf16();
+            byte_offset += c.len_utf8();
+        }
+
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn source_context() {
+        let ctx = SourceContext::new("").unwrap();
+        assert_eq!(ctx.get_line(0), None);
+        assert_eq!(ctx.offset_to_position(0), None);
+        assert_eq!(ctx.position_to_offset(SourcePosition::new(0, 0)), None);
+
+        let src = "\n \r\na";
+        let ctx = SourceContext::new(src).unwrap();
+
+        // lines
+        assert_eq!(ctx.get_line(0), Some("\n"));
+        assert_eq!(ctx.get_line(1), Some(" \r\n"));
+        assert_eq!(ctx.get_line(2), Some("a"));
+
+        // out of bounds
+        assert_eq!(ctx.offset_to_position(5), None);
+        assert_eq!(ctx.position_to_offset(SourcePosition::new(0, 1)), None);
+        assert_eq!(ctx.position_to_offset(SourcePosition::new(1, 3)), None);
+        assert_eq!(ctx.position_to_offset(SourcePosition::new(2, 1)), None);
+
+        // correct positions
+        assert_eq!(ctx.offset_to_position(1), Some(SourcePosition::new(1, 0)));
+        assert_eq!(ctx.offset_to_position(3), Some(SourcePosition::new(1, 2)));
+
+        let offset = ctx.position_to_offset(SourcePosition::new(2, 0)).unwrap();
+        assert_eq!(offset, 4);
+        assert_eq!(&src[offset as usize..], "a");
+
+        // full roundtrips
+        for offset in 0..=src.len() as u32 {
+            if let Some(sp) = ctx.offset_to_position(offset) {
+                let roundtrip = ctx.position_to_offset(sp).unwrap();
+                assert_eq!(roundtrip, offset);
+            }
+        }
+    }
+}
