fc0dbe6d9664b9a5b4774f9b6c490ce04faee783
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2026-08-26 11:35:01 -0300

    run cargo fmt

--- tests/integration.rs
+++ tests/integration.rs
@@ -56,10 +56,26 @@ fn resolves_private_method() {
     assert_eq!(
         resolved_scopes,
         [
-            (7..225, Some("new ApiConnector".into()), Some("new ApiConnector".into())),
-            (56..142, Some("ApiConnector.#t".into()), Some("ApiConnector.#makeRequest".into())),
-            (142..165, Some("ApiConnector.#r".into()), Some("ApiConnector.#buildUrl".into())),
-            (165..224, Some("ApiConnector.get".into()), Some("ApiConnector.get".into())),
+            (
+                7..225,
+                Some("new ApiConnector".into()),
+                Some("new ApiConnector".into())
+            ),
+            (
+                56..142,
+                Some("ApiConnector.#t".into()),
+                Some("ApiConnector.#makeRequest".into())
+            ),
+            (
+                142..165,
+                Some("ApiConnector.#r".into()),
+                Some("ApiConnector.#buildUrl".into())
+            ),
+            (
+                165..224,
+                Some("ApiConnector.get".into()),
+                Some("ApiConnector.get".into())
+            ),
         ]
     );
 }
